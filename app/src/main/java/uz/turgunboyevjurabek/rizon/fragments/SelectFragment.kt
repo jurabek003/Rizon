1f922c62c907d5acad4427587e38304fcdabd7d1
Asus <turgunboyevjurabek777@gmail.com>
2023-07-13 15:59:52 +0500

    starting

---
+++ app/src/main/java/uz/turgunboyevjurabek/rizon/fragments/SelectFragment.kt
@@ -0,0 +1,18 @@
+package uz.turgunboyevjurabek.rizon.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import uz.turgunboyevjurabek.rizon.R
+
+class SelectFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_select, container, false)
+    }
+}
