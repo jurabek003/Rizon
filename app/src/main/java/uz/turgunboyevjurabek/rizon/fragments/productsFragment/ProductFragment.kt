b90d814e47c77afdb72e8fabae4a747f41b6a896
newuser <ilhomjonibragimov1502@gmail.com>
2023-08-14 10:07:39 +0500

    addOrder start2

--- app/src/main/java/uz/turgunboyevjurabek/rizon/fragments/productsFragment/ProductFragment.kt
+++ app/src/main/java/uz/turgunboyevjurabek/rizon/fragments/productsFragment/ProductFragment.kt
@@ -1,19 +1,26 @@
 package uz.turgunboyevjurabek.rizon.fragments.productsFragment
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.squareup.picasso.Picasso
 import uz.turgunboyevjurabek.rizon.R
 import uz.turgunboyevjurabek.rizon.adapters.MyProductsAdapter
 import uz.turgunboyevjurabek.rizon.adapters.SelectItem
 import uz.turgunboyevjurabek.rizon.databinding.FragmentProductBinding
+import uz.turgunboyevjurabek.rizon.databinding.ItemAddOrderDialogBinding
+import uz.turgunboyevjurabek.rizon.madels.usersProducts.Product
+import uz.turgunboyevjurabek.rizon.retrofit.ApiClient
 import uz.turgunboyevjurabek.rizon.utils.AppObject
 import uz.turgunboyevjurabek.rizon.utils.MySharedPreference
 import uz.turgunboyevjurabek.rizon.utils.Status
@@ -75,9 +82,47 @@ class ProductFragment : Fragment(),SelectItem {
         product: uz.turgunboyevjurabek.rizon.madels.usersProducts.Product,
         count:Int
     ) {
+        addOrder(product, count)
+    }
 
-//        val postOrderProducts = PostOrderProducts(count, false, product, )
+    fun addOrder(product: Product, count: Int){
+        val dialog = BottomSheetDialog(binding.root.context)
+        val itemDialog = ItemAddOrderDialogBinding.inflate(layoutInflater)
 
+        itemDialog.itemPrice.text = product.price.toString()
+        itemDialog.itemName.text = product.name
+        itemDialog.tvUmumiySumma.text = "${product.price*count}"
+        Picasso.get().load("${ApiClient.PHOTO_BASE_URL}${product.photo_link}").into(itemDialog.itemImg)
+        itemDialog.tvCount.text = count.toString()
+
+        productsViewModel.getAllFilial(MySharedPreference.token)
+            .observe(viewLifecycleOwner){
+                when(it.status){
+                    Status.LOADING ->{
+                        Log.d(TAG, "onCreate: Loading")
+                        itemDialog.itemContainer.visibility = View.INVISIBLE
+                        itemDialog.myProgressBar.visibility = View.VISIBLE
+                    }
+                    Status.ERROR ->{
+                        Log.d(TAG, "onCreate: Error ${it.message}")
+                        itemDialog.myProgressBar.visibility = View.GONE
+                        Toast.makeText(context, "Error ${it.message}", Toast.LENGTH_SHORT).show()
+                    }
+                    Status.SUCCESS ->{
+                        Log.d(TAG, "onCreate: ${it.data}")
+                        itemDialog.itemContainer.visibility = View.VISIBLE
+                        itemDialog.myProgressBar.visibility = View.GONE
+                        val nameList = ArrayList<String>()
+                        it.data?.forEach {
+                            nameList.add("${it.name} ${it.address}")
+                        }
+                        itemDialog.spinnerFilial.adapter = ArrayAdapter<String>(binding.root.context, android.R.layout.simple_list_item_1, nameList)
+                    }
+                }
+            }
+
+        dialog.setContentView(itemDialog.root)
+        dialog.show()
     }
 
     override fun plusCount(
--- app/src/main/java/uz/turgunboyevjurabek/rizon/fragments/productsFragment/ProductsViewModel.kt
+++ app/src/main/java/uz/turgunboyevjurabek/rizon/fragments/productsFragment/ProductsViewModel.kt
@@ -8,7 +8,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import uz.turgunboyevjurabek.rizon.madels.filial.Filial
 import uz.turgunboyevjurabek.rizon.madels.usersProducts.GetUserProductsResponse
+import uz.turgunboyevjurabek.rizon.madels.usersProducts.post.PostProductsOrder
 import uz.turgunboyevjurabek.rizon.utils.Resource
 import uz.turgunboyevjurabek.rizon.repository.MyViewModelObjects.appRepository
 
@@ -32,8 +34,27 @@ class ProductsViewModel : ViewModel() {
         return userAllProducts
     }
 
-    private val postLiveData = MutableLiveData<Resource<GetUserProductsResponse>>()
-    fun postProductsOrder(token:String, postOrderProducts: PostOrderProducts):LiveData<Resource<GetUserProductsResponse>>{
+    private val filialLiveData = MutableLiveData<Resource<ArrayList<Filial>>>()
+    fun getAllFilial(token: String):MutableLiveData<Resource<ArrayList<Filial>>>{
+        viewModelScope.launch {
+            filialLiveData.postValue(Resource.loading("loading..."))
+            try {
+                coroutineScope {
+                    val userProducts =
+                        withContext(Dispatchers.IO) {
+                            appRepository.getAllFilials(token)
+                        }
+                    filialLiveData.postValue(Resource.success(userProducts))
+                }
+            }catch (e:Exception){
+                filialLiveData.postValue(Resource.error(e.message))
+            }
+        }
+        return filialLiveData
+    }
+
+    private val postLiveData = MutableLiveData<Resource<PostProductsOrder>>()
+    fun postProductsOrder(token:String, postOrderProducts: PostProductsOrder):LiveData<Resource<PostProductsOrder>>{
         viewModelScope.launch {
             postLiveData.postValue(Resource.loading("loading..."))
             try {
