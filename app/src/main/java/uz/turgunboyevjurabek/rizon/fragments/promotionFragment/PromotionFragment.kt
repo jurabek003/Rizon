1cd0318903568d1fb090ff91b34c4d897eaf7260
Asus <turgunboyevjurabek777@gmail.com>
2023-08-17 22:56:00 +0500

    TabLayout Finish!!!

--- app/src/main/java/uz/turgunboyevjurabek/rizon/fragments/promotionFragment/PromotionFragment.kt
+++ app/src/main/java/uz/turgunboyevjurabek/rizon/fragments/promotionFragment/PromotionFragment.kt
@@ -1,11 +1,14 @@
 package uz.turgunboyevjurabek.rizon.fragments.promotionFragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.tabs.TabLayout
 import uz.turgunboyevjurabek.rizon.R
 import uz.turgunboyevjurabek.rizon.adapters.viewPagerAdapter.ViewPagerAdapter
 import uz.turgunboyevjurabek.rizon.databinding.FragmentPromotionBinding
@@ -35,45 +38,39 @@ class PromotionFragment : Fragment() {
     private fun viewPager() {
         list= ArrayList()
         list.addAll(listOf(Pager("Junior"),Pager("Middle"),Pager("Senior")))
+
         viewPager2Adapter=ViewPagerAdapter(parentFragmentManager)
         binding.myViewPager.adapter=viewPager2Adapter
+        binding.myViewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout))
         binding.tabLayout.setupWithViewPager(binding.myViewPager)
 
-
         val tabCount=binding.tabLayout.tabCount
-
 
         for (i in 0 until tabCount){
             val itemTabBinding = ItemTabBinding.inflate(layoutInflater)
             val tab=binding.tabLayout.getTabAt(i)
             itemTabBinding.tabItemName.text=list[i].name.toString()
-            tab?.text=itemTabBinding.tabItemName.text.toString()
             tab?.setCustomView(itemTabBinding.root)
         }
-        val itemTabBinding=ItemTabBinding.inflate(layoutInflater)
-     /*  binding.tabLayout.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
 
+     binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
             @SuppressLint("ResourceAsColor")
             override fun onTabSelected(tab: TabLayout.Tab?) {
 
-                itemTabBinding.tabItemName.text=list[tab?.position!!].name.toString()
-                itemTabBinding.tabItemName.alpha=1f
-                tab.text=itemTabBinding.tabItemName.text.toString()
-                tab.customView=itemTabBinding.tabItemName
+                val tabBinding = ItemTabBinding.bind(tab?.customView!!)
+                tabBinding.tabItemName.alpha=1f
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-                itemTabBinding.tabItemName.text=list[tab?.position!!].name.toString()
-                tab.text=itemTabBinding.tabItemName.text.toString()
-                tab.customView=itemTabBinding.tabItemName
+                val tabBinding = ItemTabBinding.bind(tab?.customView!!)
+                tabBinding.tabItemName.alpha=.5f
             }
 
             override fun onTabReselected(tab: TabLayout.Tab?) {
                 Toast.makeText(requireContext(), "reselected", Toast.LENGTH_SHORT).show()
             }
         })
-*/
+
 
 
     }
